7bf0e2289bfc89f43cf4b4a38a408a0820803dea
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-03 15:23:26 +0100

    feat: personalised user greeting using username and github name

--- src/main/java/com/myorg/SecurityPlayground/controller/SecurityController.java
+++ src/main/java/com/myorg/SecurityPlayground/controller/SecurityController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +24,10 @@ public class SecurityController {
     }
 
     @GetMapping("/protected/greeting")
-    public ResponseEntity<String> protectedGreeting() {
-        return new ResponseEntity<String> ("Welcome back our dear, valued, wonderful user", HttpStatusCode.valueOf(200));
+    public ResponseEntity<String> protectedGreeting(@AuthenticationPrincipal OAuth2User user) {
+        String userName = user.getAttribute("name");
+        String githubUserName = user.getAttribute("login");
+        return new ResponseEntity<String> ("Welcome back our dear, valued, wonderful user " + userName + " AKA " + githubUserName, HttpStatusCode.valueOf(200));
     }
 
 }

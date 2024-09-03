0c7b49fafc944e13feec1df40bad6391e44a47ae
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-09-03 12:30:24 +0100

    feat: add to security config

--- src/main/java/com/myorg/SecurityPlayground/security/SecurityConfig.java
+++ src/main/java/com/myorg/SecurityPlayground/security/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.myorg.SecurityPlayground.security;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +17,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> {
             auth.requestMatchers("api/v1/open/greeting").permitAll();
-            auth.requestMatchers("api/v1/protected/greeting").authenticated();
+            auth.requestMatchers("api/v1/protected/greeting").authenticated().toString(e -> e
+                            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                    )
+                    .oauth2Login();
         });
         return http.build();
     }
